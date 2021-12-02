1b65b3636ae9aca61539f26221b6a5981b73f864
Severyn Kurach <severynson.kurach@gmail.com>
2021-12-02 09:34:21 +0200

    10 Lecture Project Done

---
+++ 10 Lecture Project/src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React, {useContext} from 'react';
+import AuthContext from '../store/auth-context';
+import Button from '../UI/Button/Button';
+
+import Card from '../UI/Card/Card';
+import classes from './Home.module.css';
+
+const Home = (props) => {
+  const AuthCtx = useContext(AuthContext);
+
+  return (
+    <Card className={classes.home}>
+      <h1>Welcome back!</h1>
+      <Button onClick={AuthCtx.onLogout}>Logout</Button>
+    </Card>
+  );
+};
+
+export default Home;
