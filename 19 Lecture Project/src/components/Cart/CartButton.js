b28887f804090756b1f695c4bbab8f61da2f24d9
Severyn Kurach <severynson.kurach@gmail.com>
2021-12-22 23:44:46 +0200

    books adding now and - too

--- 19 Lecture Project/src/components/Cart/CartButton.js
+++ 19 Lecture Project/src/components/Cart/CartButton.js
@@ -1,9 +1,11 @@
 import classes from './CartButton.module.css';
-import { useDispatch } from 'react-redux';
+
+import { useDispatch, useSelector } from 'react-redux';
 import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector(state => state.cart.totalQuantity);
 
   const toggleCartHandler = (e) => {
     e.preventDefault();
@@ -13,7 +15,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
